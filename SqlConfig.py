be0fc043cc83caab66b6d8451aac71b120963f87
Rishy <271669@usn.no>
2025-03-05 11:14:55 +0100

    test commit

--- SqlConfig.py
+++ SqlConfig.py
@@ -1,1 +1,12 @@
-#
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root"
+)
+
+cursorObject = connection.cursor()
+cursorObject.execute("CREATE DATABASE Sensordata")
+connection.close()
+

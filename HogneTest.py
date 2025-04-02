c168f02e07203f01cbda0290c3cdd632bca1dccb
Hogne <274822@usn.no>
2025-04-02 08:39:33 +0200

    test commit

--- HogneTest.py
+++ HogneTest.py
@@ -18,19 +18,33 @@ class SensorDataCollector:
         self.conn = pymysql.connect(**db_config)
         self.cursor = self.conn.cursor()
 
+        self.acceleration_x2 = 0
+        self.acceleration_y2 = 0
+        self.acceleration_z2 = 0
+
     def sendCommand(self, command):
         command_json = json.dumps({"Command": command})
         self.ser.write(command_json.encode())
         print(f"Sent: {command_json}")
 
-    def insertTempratureData(self,sensor_id, temperature):
+    def insertTemperatureData(self,sensor_id, temperature):
         timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
         self.cursor.execute('''
         INSERT INTO temperaturereadings (sensor_id, timestamp, temperature)
         VALUES (%s, %s, %s)
         ''', (sensor_id, timestamp, temperature))
-        self.conn.commit()
 
+    def insertAccelerationData(self, sensor_id, acceleration_x, acceleration_y, acceleration_z, diff_acceleration_x, diff_acceleration_y, diff_acceleration_z):
+        timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+
+        self.cursor.execute('''
+        INSERT INTO accelerationreadings (sensor_id, timestamp, acceleration_x, acceleration_y, acceleration_z, diff_acceleration_x, diff_acceleration_y, diff_acceleration_z)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        ''', (sensor_id,timestamp, acceleration_x, acceleration_y, acceleration_z, diff_acceleration_x, diff_acceleration_y, diff_acceleration_z))
+
+
+        self.conn.commit()
+        print('temprature transfered')
     def processData(self, data):
         try:
             dataJson = json.loads(data)
@@ -38,30 +52,54 @@ class SensorDataCollector:
             print(dataJson)
             if "acceleration" and "temperature" in dataJson:
                 print("hAR DATA A OG T")
+
                 temperature_stamp = dataJson['temperature']
-                print(temperature_stamp)
+                sensor_id_temp = temperature_stamp['sensor_id']
                 temperature = temperature_stamp['temperature']
-                #sensor_id_temp
 
                 acceleration = dataJson['acceleration']
+                sensor_id_acc = acceleration['sensor_id']
                 acceleration_x = acceleration['x']
                 acceleration_y = acceleration['y']
                 acceleration_z = acceleration['z']
 
-                #self.insertTempratureData(self.sensor_id, temperature)
+                diff_acceleration_x = self.acceleration_x2 - acceleration_x
+                diff_acceleration_y = self.acceleration_y2 - acceleration_y
+                diff_acceleration_z = self.acceleration_z2 - acceleration_z
+
+                self.acceleration_x2 = acceleration_x
+                self.acceleration_y2 = acceleration_y
+                self.acceleration_z2 = acceleration_z
+
+                #self.insertTemperatureData(sensor_id_temp, temperature)
+                #self.insertAccelerationData(sensor_id_acc,acceleration_x, acceleration_y, acceleration_z, diff_acceleration_x, diff_acceleration_y, diff_acceleration_z)
 
             elif "acceleration" in dataJson:
                 print("aks")
                 acceleration = dataJson['acceleration']
+                sensor_id_acc = acceleration['sensor_id']
                 acceleration_x = acceleration['x']
                 acceleration_y = acceleration['y']
                 acceleration_z = acceleration['z']
 
+                diff_acceleration_x = self.acceleration_x2 - acceleration_x
+                diff_acceleration_y = self.acceleration_y2 - acceleration_y
+                diff_acceleration_z = self.acceleration_z2 - acceleration_z
+
+                self.acceleration_x2 = acceleration_x
+                self.acceleration_y2 = acceleration_y
+                self.acceleration_z2 = acceleration_z
+                print(acceleration_z, acceleration_x, acceleration_y)
+                print(diff_acceleration_x, diff_acceleration_y, diff_acceleration_z)
+
+                self.insertAccelerationData(sensor_id_acc,acceleration_x, acceleration_y, acceleration_z, diff_acceleration_x, diff_acceleration_y, diff_acceleration_z)
+
+
+
             elif "temperature" in dataJson:
                 print("temp")
                 temprature_stamp = dataJson['temperature']
-                print(temprature_stamp)
-                temprature = temprature_stamp['temperature']
+                temperature = temprature_stamp['temperature']
 
             else:
                 print("no valid data recived")
@@ -101,7 +139,7 @@ db_config = {
 frequency = 1  # For example, 10 Hz
 
 collector = SensorDataCollector(port='COM5', baudrate=9600, db_config=db_config, frequency=frequency)
-#collector.send_command("START")
+collector.sendCommand("START")
 collector.run()
 
 #time.sleep(30)

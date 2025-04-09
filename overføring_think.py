3856702471dc854d0ebb66287e053120fcae4fe5
Rishy <271669@usn.no>
2025-04-09 11:58:16 +0200

    endra

--- overføring_think.py
+++ overføring_think.py
@@ -1,20 +1,22 @@
+import time
 import mysql.connector  # Importerer MySQL-connektoren
-import numpy as np
+
+
 
 class HentData:
     def __init__(self):
-        self.diff_acceleration_x = {dx}
-        self.diff_acceleration_y = {dy}
-        self.diff_acceleration_z = {dz}
+        self.diff_acceleration_x = 0
+        self.diff_acceleration_y = 0
+        self.diff_acceleration_z = 0
 
-        self.
+        self.temperatur = 0
 
 
         try:
             self.db = mysql.connector.connect(
                 host="localhost",  # Kobler til MySQL-serveren på localhost
                 user="root",  # Brukernavn for databasen
-                password="root",  # Passord for databasen
+                password="Rishaa24",  # Passord for databasen
                 database="sensordata"  # Spesifiserer databasen som skal brukes
             )
             self.cursor = self.db.cursor(dictionary=True)  # Oppretter en cursor for å utføre spørringer
@@ -29,29 +31,37 @@ class HentData:
                 "SELECT temperature FROM sensordata.temperaturereadings WHERE sensor_id = 1 LIMIT 1;")  # Utfører SQL-spørring
             return self.cursor.fetchall()  # Henter alle resultatene fra spørringen
         return None  # Returnerer None hvis spørringen mislykkes
+
     def hent_diffacc(self):
         if self.cursor:  # Sjekker om tilkobling til databasen er vellykket
                 self.cursor.execute(
                     "SELECT diff_acceleration_x,diff_acceleration_y,diff_acceleration_z FROM sensordata.accelerationreadings WHERE sensor_id = 2 LIMIT 1;")  # Utfører SQL-spørring
                 return self.cursor.fetchall()  # Henter alle resultatene fra spørringen
-        result = self.cursor.fetchall()
-        if result:
-            dx, dy, dz = result
-            print(f"diff_acceleration_x{dx}")
-            print(f"diff_acceleration_x{dy}")
-            print(f"diff_acceleration_x{dz}")
 
-        else:
-            print("Ingen acc data funnet")
-            return None  # Returnerer None hvis spørringen mislykkes
+    def return_data(self):
+
+        return {
+            "temperature": self.temperatur,
+            "x": self.diff_acceleration_x,
+            "y": self.diff_acceleration_y,
+            "z": self.diff_acceleration_z
+        }
+
+    def run(self):
+
+        while True:
+
+            diff_acceleartion = self.hent_diffacc()
+            self.diff_acceleration_x = diff_acceleartion[0]['diff_acceleration_x']
+            self.diff_acceleration_y = diff_acceleartion[0]['diff_acceleration_y']
+            self.diff_acceleration_z = diff_acceleartion[0]['diff_acceleration_z']
+
+            temperatur = self.hent_temperatur()
+            self.temperatur = temperatur[0]['temperature']
 
 
-# Eksempel på bruk av klassen
+
 data_henter = HentData()
-temperatur = data_henter.hent_temperatur()  # Henter temperaturdata fra MySQL
-diff_acceleration_x = data_henter.hent_diffacc()
-data_henter.send_til_thinkter(temperatur)  # Sender hentet data til Thinkter-serveren
 
-"""
 
-"""
+data_henter.run()
